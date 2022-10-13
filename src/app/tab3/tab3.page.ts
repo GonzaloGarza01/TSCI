632628b43e77856b56b9feaf388b82dfafb589e1
GonzaloGarza01 <gonzalo.garza01@hotmail.com>
2022-10-13 00:15:54 -0500

    [dev] +funcionalidad para detectar el tipo de cuenta / +interfaz de welcome / +cambios en las rutas

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
-
+import { child, get, getDatabase, onValue, ref,  } from 'firebase/database';
+import { getAuth } from "firebase/auth";
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-
+export class Tab3Page implements OnInit {
+  uid;
+  dataUser;
   constructor(
     private authSvc:AuthService,
     private toastController: ToastController,
     private router: Router,
   ) {}
+
+
+  ngOnInit() {
+    const auth = getAuth();
+    this.uid = auth.currentUser.uid;
+    this.getUserInfo();
+  }
 
   logOut(){
     this.authSvc.logout();
@@ -31,8 +40,20 @@ export class Tab3Page {
   }
 
   redirectUser(){
-    this.router.navigate(['register']);
+    this.router.navigate(['login']);
   }
 
+  getUserInfo(){
+    const db = getDatabase();
+    const dbRef = ref(db, 'users/' + this.uid);
+    onValue(dbRef, (snapshot) => {
+      //Traer el rol de la BD
+      if (snapshot.exists()) {
+        this.dataUser = snapshot.val();
+      } else {
+        console.log("No data available");
+      }
+    });
+  }
   
 }
