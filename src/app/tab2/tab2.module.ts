a124867cad63ce9b46a07dc6022853ae11c7ed50
GonzaloGarza01 <gonzalo.garza01@hotmail.com>
2022-10-17 22:07:22 -0500

    [dev] +Modal avisos / +Cambio en el fondo de cada interfaz / +Interfaz avisos funcional

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { ModalAvisosComponent } from '../components/modal-avisos/modal-avisos.component';
 
 @NgModule({
   imports: [
@@ -14,6 +15,6 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     FormsModule,
     Tab2PageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page, ModalAvisosComponent]
 })
 export class Tab2PageModule {}
