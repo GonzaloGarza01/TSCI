1a4fe6af65dd41b1620d1c509a9479601cbaa5d6
GonzaloGarza01 <gonzalo.garza01@hotmail.com>
2022-10-19 16:22:04 -0500

    [dev] Cambios en las rutas de la base de datos

--- src/app/components/modal-alumnos/modal-alumnos.component.ts
+++ src/app/components/modal-alumnos/modal-alumnos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, ToastController } from '@ionic/angular';
-import { getDatabase, ref, set } from 'firebase/database';
+import { getDatabase, push, ref, set, onValue } from 'firebase/database';
+import { getAuth } from "firebase/auth";
 
 @Component({
   selector: 'app-modal-alumnos',
@@ -10,21 +11,73 @@ import { getDatabase, ref, set } from 'firebase/database';
 export class ModalAlumnosComponent implements OnInit {
 
   alumnos: any ={};
+  grupos: any = {};
+  gruposArray: Array<any>;
+  gruposNombre: Array<any> = [];
+  uid;
 
   constructor(
     private modalController: ModalController,
     private toastController: ToastController,
-
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const auth = getAuth();
+    this.uid = auth.currentUser.uid;
+    this.getGrupos();
+  }
+
+  getGrupos(){
+    const db = getDatabase();
+    const usersRef = ref(db, `users/${this.uid}/grupos`);
+    onValue(usersRef, (snapshot) => {
+      this.gruposNombre = [];
+      this.gruposArray = snapshot.val();
+      Object.keys(this.gruposArray).forEach(key => {
+        this.gruposNombre.push(this.gruposArray[key].nombre);
+        console.log(this.gruposArray[key].nombre);
+      });
+      console.log(this.gruposNombre);
+    });
+  }
+
+  async tareasSection(){
+    const tareas = document.querySelector('#section-tareas');
+    const calif = document.querySelector('#section-calificaciones');
+
+    tareas.classList.add('selected');
+    calif.classList.remove('selected');
+
+    const tSection = document.querySelector('#tareas-container');
+    const cSection = document.querySelector('#calif-container');
+
+    tSection.classList.add('selected-container');
+    cSection.classList.remove('selected-container');
+
+  }
+
+  async calificacionesSection(){
+    const tareas = document.querySelector('#section-tareas');
+    const calif = document.querySelector('#section-calificaciones');
+
+    tareas.classList.remove('selected');
+    calif.classList.add('selected');
+
+    const tSection = document.querySelector('#tareas-container');
+    const cSection = document.querySelector('#calif-container');
+
+    tSection.classList.remove('selected-container');
+    cSection.classList.add('selected-container');
+
+  }
   
   async onSave(){
     try {
       if(this.alumnos.name && this.alumnos.edad && this.alumnos.number && this.alumnos.extra){
         const db = getDatabase();
-        set(ref(db, 'alumnos/' + this.alumnos.name), {
+        set(ref(db, `users/${this.uid}/grupos/${this.alumnos.grupo}/alumnos/${this.alumnos.name}`), {
           nombre: this.alumnos.name,
+          grupo: this.alumnos.grupo,
           edad: this.alumnos.edad,
           contacto: this.alumnos.number,
           comentarios: this.alumnos.extra
@@ -35,7 +88,32 @@ export class ModalAlumnosComponent implements OnInit {
           this.alumnos.edad = '',
           this.alumnos.number = '',
           this.alumnos.extra = ''
-          this.dismissModal();
+        });
+      }
+      else{
+        console.log("No existe informacion")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async onSaveGrupo(){
+    try {
+      if(this.grupos.name){
+        const db = getDatabase();
+        const usersRef = ref(db, 'users/' + this.uid + '/grupos/')
+        const pushData = push(usersRef)
+        const id  = pushData.key;
+        console.log(id);
+        set(ref(db, 'users/' + this.uid + '/grupos/' + this.grupos.name), {
+          //id: crear id,
+          nombre: this.grupos.name,
+          id: id,
+        })
+        .then(()=>{
+          this.presentToast('Grupo registrado');
+          this.grupos.name = ''
         });
       }
       else{
