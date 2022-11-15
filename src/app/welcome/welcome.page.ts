59699ae3c169d7daf48175cf7f12d8ddc78e58ae
GonzaloGarza01 <gonzalo.garza01@hotmail.com>
2022-11-14 20:12:58 -0600

    [dev] Cambios en los input / Mejor diseño en login y register

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  goLogin(){
+    this.router.navigate(['login']);
+  }
+
+  goRegister(){
+    this.router.navigate(['register']);
   }
 
 }
